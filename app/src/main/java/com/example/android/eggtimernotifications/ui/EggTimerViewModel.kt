d7ae3b3476ccd288e8982e7f1d4a55fa2c3bdde1
Wayno1979 <waynecockram@gmail.com>
2022-01-19 14:40:34 +0000

    clean up notification usage

--- app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerViewModel.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerViewModel.kt
@@ -118,12 +118,6 @@ class EggTimerViewModel(private val app: Application) : AndroidViewModel(app) {
                 }
                 val triggerTime = SystemClock.elapsedRealtime() + selectedInterval
 
-                //get an instance of NotificationManager and call sendNotification
-                val notificationManager = ContextCompat.getSystemService(
-                    app, NotificationManager::class.java
-                ) as NotificationManager
-                notificationManager.sendNotification(app.getString(R.string.timer_running), app)
-
                 // TODO: Step 1.15 call cancel notification
 
                 AlarmManagerCompat.setExactAndAllowWhileIdle(
